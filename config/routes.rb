1ad200ccfc7fadf9f4a4a1013984c614196be401
yanagi_prg <takayanagi.prg@gmail.com>
2018-12-03 15:37:16 +0900

    some change on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/indez'
+  get '/' => 'home#indez' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
